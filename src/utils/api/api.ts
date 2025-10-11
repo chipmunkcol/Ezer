67ca14b209151008d21aa0faddb0699378436fcb
chipmunkcol <jack95711@gmail.com>
2025-10-11 14:54:26 +0900

    feat: 멤버 api 설정

---
+++ src/utils/api/api.ts
@@ -0,0 +1,85 @@
+import api, { type ErrorRes } from "./instance";
+
+export const getMemers = async () => {
+  try {
+    const response = await api.get("/admin/members");
+    return response.data;
+  } catch (error) {
+    const err = error as ErrorRes;
+    throw {
+      status: err.error.status,
+      message: err.error.message,
+    };
+  }
+};
+
+/**
+ *
+ * @param Member 등록할 멤버 정보
+ * @returns 생성된 멤버 데이터
+ */
+export const postMember = async (Member: Member): Promise<ResponseMember> => {
+  try {
+    const response = await api.post("/admin/members", Member);
+    return response.data;
+  } catch (error) {
+    const err = error as ErrorRes;
+    throw {
+      status: err.error.status,
+      message: err.error.message,
+    };
+  }
+};
+
+export const updateMember = async ({
+  id,
+  member,
+}: {
+  id: string;
+  member: Member;
+}): Promise<ResponseMember> => {
+  try {
+    const response = await api.put(`/admin/members/${id}`, member);
+    return response.data;
+  } catch (error) {
+    const err = error as ErrorRes;
+    throw {
+      status: err.error.status,
+      message: err.error.message,
+    };
+  }
+};
+
+export const deleteMember = async (id: string): Promise<void> => {
+  try {
+    await api.delete(`/admin/members/${id}`);
+  } catch (error) {
+    const err = error as ErrorRes;
+    throw {
+      status: err.error.status,
+      message: err.error.message,
+    };
+  }
+};
+
+interface Member {
+  name: string;
+  position: "SAINT"; // | 'DEACON' | 'DEACONESS' | 'MEMBER'
+  barnabasEducation: "COMPLETED"; // | 'NOT_COMPLETED'
+  baptism: "RECEIVED"; // | 'NOT_RECEIVED' | 'UNDECIDED'
+  discipleship: "COMPLETED"; // | 'NOT_COMPLETED'
+  gender: "MALE"; // | 'FEMALE'
+  phone: string;
+  birthDate: string; // YYYY-MM-DD
+  cellId: null; // | number
+  familyId: null; // | number
+  barnabasName: string;
+  registeredAt: string; // YYYY-MM-DD
+  note: string;
+}
+
+interface ResponseMember extends Member {
+  id: string;
+  createdAt: string; //"2025-01-01T10:00:00.000Z",
+  updatedAt: string; // "2025-01-01T10:00:00.000Z"
+}
---
+++ src/utils/api/instance.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+// 에러 응답 타입
+export interface ErrorRes {
+  success: false;
+  error: {
+    code: string; // "INTERNAL_SERVER_ERROR";
+    message: string; // "API is not implemented";
+    status: number; // 500;
+    timestamp: string; // "2025-10-11T04:34:54.466Z";
+    path: string; // "/admin/members";
+    details: null;
+  };
+}
+
+const api = axios.create({
+  baseURL: "https://ezeryoungcouple-dev.duckdns.org",
+  // maxRedirects: 3,
+});
+
+// 요청 인터셉터 추가하기
+axios.interceptors.request.use(
+  function (config) {
+    // 요청이 전달되기 전에 작업 수행
+    return config;
+  },
+  function (error) {
+    // 요청 오류가 있는 작업 수행
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터 추가하기
+axios.interceptors.response.use(
+  function (response) {
+    // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+    // 응답 데이터가 있는 작업 수행
+    return response;
+  },
+  function (error) {
+    // 2xx 외의 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+    // 응답 오류가 있는 작업 수행
+    const err: ErrorRes = error;
+    console.error("API Error:", err);
+    return Promise.reject(err);
+  }
+);
+
+export default api;
